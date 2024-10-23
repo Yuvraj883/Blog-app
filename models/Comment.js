800483a494ab77b63b2b5de5c021f1847a70f46b
Yuvraj <devyuvraj883@gmail.com>
2024-10-24 02:34:12 +0530

    Created comment schema

---
+++ models/Comment.js
@@ -0,0 +1,20 @@
+const {Schema, model} = require('mongoose');
+
+const commentSchema = Schema({
+  content: {
+    type:String,
+    required:true
+  },
+  createdBy:{
+    type: Schema.Types.ObjectId,
+    ref:'User'
+
+  },
+  blogPost:{
+    type:Schema.Types.ObjectId,
+    ref:'Blog'
+  }
+})
+
+const Comment = model('comment', commentSchema);
+module.exports = Comment;
